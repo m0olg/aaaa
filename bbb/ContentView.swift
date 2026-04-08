b9e369b551f754769359048fa62a6642442322a8
ssss <ss>
2026-04-08 18:27:34 +0900

    Initial Commit

---
+++ bbb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bbb
+//
+//  Created by 옥물결 on 4/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bbb/bbbApp.swift
@@ -0,0 +1,17 @@
+//
+//  bbbApp.swift
+//  bbb
+//
+//  Created by 옥물결 on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct bbbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
