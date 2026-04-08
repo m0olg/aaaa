0cf3086cc2c240f4dd057115e121a976b801457e
ssss <ss>
2026-04-08 19:51:58 +0900

    1

--- bbb/ContentView.swift
+++ bbb/ContentView.swift
@@ -9,16 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack() {
+            Color.black.ignoresSafeArea()
+            
+            VStack() {
+                HStack() {
+                    Image("book_on")
+                        .padding(.leading, 9)
+                }
+                
+            }
+            
+            
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
