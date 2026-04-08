1717986aa5e03823db66fabc21ac023a8c6602f1
ssss <ss>
2026-04-09 00:19:49 +0900

    4

--- bbb/ContentView.swift
+++ bbb/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         ZStack() { // 바탕 검정
             Color.black.ignoresSafeArea()
             
-            VStack() {
+            VStack(spacing:0) {
                 HStack() {
                     Image("book_on")
                         .padding(.leading, 9)
